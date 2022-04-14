475cbe5a51732ac5785f166e5bb446380f0ff238
ankita-2021 <ankitachakrawarti@gmail.com>
2022-04-14 16:32:51 +0530

    myassignment5submit

---
+++ Assignment-4/code/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "./LandPage/Home";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Postview from "./Postview";
+
+function App(){
+    return(
+        <div className="wrapper">
+            <Router>
+                <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/Postview" element={<Postview/>}/>
+                </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default App;
